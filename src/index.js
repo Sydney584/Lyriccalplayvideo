2ac76c4c9e08bef0fc2c0144c58a931ef8206e64
sydney <sydneychase5841@gmail.com>
2021-05-01 10:55:02 -0400

    adding html, js and css

---
+++ src/index.js
@@ -0,0 +1,78 @@
+// getting data
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+
+const BASE_URL = 'https://api.lyrics.ovh';
+// create search function returns object of arrays
+async function searchSongs(term) {
+  const res = await fetch(`${BASE_URL}/suggest/${term}`);
+  const data = await res.json();
+  
+  showData(data);
+}
+// Showing the Song and Artist/Group in the Dom
+function showData(data) {
+  let output = '';
+
+  data.data.forEach(song => {
+    output += `
+    <li>
+    <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+    <button class="btn" data-artist="${song.artist.name}"
+    data-songtitle="${song.title}">Get Lyrics</button>
+
+    </li>
+    `;
+
+  });
+
+  result.innerHTML = `
+  <ul class="songs">
+  ${output}
+  </ul>
+  
+  `;
+}
+// Create getLyrics for song function to display lyrics to DOM
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${BASE_URL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`;
+}
+
+// Creating an Event Listener for the form
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if(!searchTerm) {
+    alert('Please Type your search request! ');
+  }
+  else {
+    searchSongs(searchTerm);
+  }
+
+});
+
+
+
+// Creating Event Listener for get lyrics link to show lyrics
+
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+    
+    getLyrics(artist, songTitle);
+  }
+});
+
+
